26153b558f46412537af365230ef6f36d70d9baf
Seba010d <Seba010d@gmail.com>
2025-12-04 10:11:52 +0100

    feat: implement user authentication with login functionality and JWT token generation

--- my-api/src/controllers/userController.ts
+++ my-api/src/controllers/userController.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from 'express';
 import { prisma } from '../prisma.js';
+import bcrypt from 'bcrypt'
 
+/**
+ * Method Get Records
+ * @param req 
+ * @param res 
+ * @returns Array
+ */
 export const getRecords = async (req: Request, res: Response) => {
   try {
     const data = await prisma.user.findMany();
@@ -11,94 +18,120 @@ export const getRecords = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * Method Get Record
+ * @param req 
+ * @param res 
+ * @returns Object
+ */
 export const getRecord = async (req: Request, res: Response) => {
-  const id = Number(req.params.id)
+  const id = Number(req.params.id);
 
-  if (!id) {
-    return res.status(400).json({ error: 'Id has no value' });
+  if(!id) {
+    return res.status(400).json({ error: 'Id is missing '})
   }
 
   try {
     const data = await prisma.user.findUnique({
-      where: {
-        id
-      }
-    });
-    return res.status(200).json(data);
+      where: { id },
+    })
+    return res.status(200).json(data)
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Failed to fetch user' });
-  }
-};
-
-
-export const createRecord = async (req: Request, res: Response) => {
-
-  const { name, email, organisationId } = req.body
-
-  if (!name || !email || !organisationId ) {
-    return res.status(400).json({ error: 'All data is required' });
-  }
-
-  try {
-    const data = await prisma.user.create({
-      data: { 
-        name,
-        email,
-        organisationId: Number(organisationId)
-      }
-    })
-
-    return res.status(201).json(data);
-
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ error: 'Something went wrong' });
+    res.status(500).json({ error: 'Failed to fetch car' });
   }
 }
 
-export const updateRecord = async (req: Request, res: Response) => {
-  const id = Number(req.params.id)
-  const { name, email, organisationId } = req.body
+/**
+ * Method Create Record
+ * @param req 
+ * @param res 
+ * @returns Object
+ */
+export const createRecord = async (req: Request, res: Response) => {
 
-  if (!id) {
-    return res.status(400).json({ error: 'Id has no value' });
+  const { firstname, lastname, email, password, role, isActive } = req.body;
+  
+  if(!firstname || !lastname || !email || !password || !role || !isActive) {
+    return res.status(400).json({ error: 'All data is required' })
+  }
+  
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const data = await prisma.user.create({
+      data: {
+        firstname,
+        lastname,
+        email,
+        password: hashedPassword,
+        role,
+        isActive: Boolean(isActive)
+      }
+    })
+    return res.status(201).json(data)
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Something went wrong' }) 
+  }
+}
+
+/**
+ * Method Update Record
+ * @param req 
+ * @param res 
+ * @returns Object
+ */
+export const updateRecord = async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+
+  if(!id) {
+    return res.status(400).json({ error: 'Id is missing '})
   }
 
-  if (!name || !email || !organisationId) {
-    return res.status(400).json({ error: 'All data is required' });
+  const { firstname, lastname, email, role, isActive } = req.body;
+  
+  if(!firstname || !lastname || !email || !role || !isActive) {
+    return res.status(400).json({ error: 'All data is required' })
   }
 
   try {
     const data = await prisma.user.update({
       where: { id },
       data: {
-        name,
+        firstname,
+        lastname,
         email,
-        organisationId: Number(organisationId)
+        role,
+        isActive: Boolean(isActive)
       }
     })
-    return res.status(201).json(data);
+    return res.status(201).json(data)
   } catch (error) {
-    console.log(error);
-    return res.status(500).json({ error: 'Something went wrong' });
+    console.error(error);
+    return res.status(500).json({ error: 'Something went wrong' }) 
   }
 }
 
 export const deleteRecord = async (req: Request, res: Response) => {
   const id = Number(req.params.id)
 
-  if (!id) {
-    return res.status(400).json({ error: 'Id has no value' });
+  if(!id) {
+    return res.status(400).json({ error: 'Id is missing '})
   }
-
+  
   try {
     const data = await prisma.user.delete({
       where: { id }
     })
-    res.status(200).json({ message: 'Record deleted' });
+    res.status(200).json({ 
+      message: 'Record deleted',
+      deletedId: id
+    })
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Failed to delete record' });
+    return res.status(500).json({ error: 'Failed to delete record' }) 
   }
+  
 }
+
